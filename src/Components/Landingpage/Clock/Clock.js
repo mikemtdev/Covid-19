8dcbbc44e0a7982d44f4aaab65d6402b5df3f31d
mikemnjovu <mikemnjovu@gmail.com>
2020-04-21 22:10:28 +0200

    Add api to topleading component and map over array and conditionally render it

--- src/Components/Landingpage/Clock/Clock.js
+++ src/Components/Landingpage/Clock/Clock.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './Clock.css';
 import axios from 'axios';
 const Clock = () => {
@@ -7,42 +7,54 @@ const Clock = () => {
 	const [Deaths, setDeaths] = useState(null);
 	const [UpDate, setUpDate] = useState(null);
 	const [Recovered, setRecovered] = useState(null);
-	const apiFetchComfirmed = axios.get('https://covid2019-api.herokuapp.com/v2/total')
+	const apiFetchComfirmed = axios
+		.get('https://covid2019-api.herokuapp.com/v2/total')
 		.then(resp => {
 			const confirmedCases = resp.data.data.confirmed;
-			setConfirmed(confirmedCases)
+			setConfirmed(confirmedCases);
 
-			const lastUpdateDate = resp.data.dt
-			setUpDate(lastUpdateDate)
+			const lastUpdateDate = resp.data.dt;
+			setUpDate(lastUpdateDate);
 
 			const activeCases = resp.data.data.active;
-			setActive(activeCases)
+			setActive(activeCases);
 
 			const recoveredCases = resp.data.data.recovered;
-			setRecovered(recoveredCases)
+			setRecovered(recoveredCases);
 
-			const DeathCases = resp.data.data.deaths
-			setDeaths(DeathCases)
-
-			
+			const DeathCases = resp.data.data.deaths;
+			setDeaths(DeathCases);
 		})
-	
-
-
+		.catch(err => console.log('err'));
 
 	return (
 		<div className='clockmain-Container'>
 			<div className='clock-container'>
-				<h1 className='clock-h1'>{Confirmed? `Comfirmed ${Confirmed}`: 'Please Wait'}</h1>
-				<h3 className='clock-h1'>{Active?`Infected ${Active}` : 'Please Wait'}</h3>
-				<h3 className='clock-h1'>{Recovered ? `Recovered ${Recovered}` : 'Please Wait'}</h3>
-				<h3 className='clock-h1'>{Deaths?`Deaths ${Deaths}`: 'Please Wait'}</h3>
+				<h1 className='clock-h1'>
+					{Confirmed
+						? `Comfirmed ${Confirmed}`
+						: 'Please wait....'}
+				</h1>
+				<h3 className='clock-h1'>
+					{Active
+						? `Infected ${Active}`
+						: 'Please wait....'}
+				</h3>
+				<h3 className='clock-h1'>
+					{Recovered
+						? `Recovered ${Recovered}`
+						: 'Please wait....'}
+				</h3>
+				<h3 className='clock-h1'>
+					{Deaths ? `Deaths ${Deaths}` : 'Please wait....'}
+				</h3>
 				{/* <h2 className='clock-h1'>
 					{ `Leading Country `}
 				</h2> */}
 
 				<p className='clock-h1'>
-					<h3>Last Update date</h3> {UpDate? UpDate: 'Please Wait'}
+					<h3>Last Update date</h3>{' '}
+					{UpDate ? UpDate : 'Please wait....'}
 				</p>
 			</div>
 		</div>
