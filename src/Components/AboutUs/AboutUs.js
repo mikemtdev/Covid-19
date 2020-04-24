731f202a0fc14b8d370912b34807d861f57bcd1d
mikemnjovu <mikemnjovu@gmail.com>
2020-04-24 13:36:22 +0200

    fix over Flow about us p constent

--- src/Components/AboutUs/AboutUs.js
+++ src/Components/AboutUs/AboutUs.js
@@ -20,38 +20,6 @@ const AboutUs = () => {
 				suscipit explicabo maxime hic facilis ad sequi quod,
 				eligendi perferendis laboriosam vero impedit
 				aspernatur minus voluptatum, tempore distinctio.
-				Error soluta possimus fugiat laboriosam animi ut
-				unde reiciendis adipisci quisquam tempore sit
-				accusantium, nostrum voluptatibus assumenda repellat
-				ipsam inventore quos enim cum minima eum aspernatur
-				dolores cumque? Maiores vitae ratione modi unde
-				reiciendis corporis minus, quidem sequi vero iure
-				ipsam laboriosam nemo. Beatae, dolor modi? Saepe
-				illo maxime, laudantium ducimus in sit asperiores
-				explicabo repellat deleniti assumenda esse vitae
-				molestiae nesciunt vel suscipit quibusdam accusamus
-				laboriosam, sint dicta magni, aliquid quod animi
-				perspiciatis. Natus, voluptate. Non exercitationem
-				doloribus quis aperiam perspiciatis! Ipsa excepturi
-				illo eos iure, vitae ipsam maxime dolorum, quidem
-				tempore ducimus incidunt optio voluptatibus totam
-				cumque eligendi architecto ratione eum vero id
-				voluptatum minus saepe velit soluta. Ex, neque
-				alias? Error, ducimus. Debitis, quae ut similique
-				pariatur incidunt quaerat dolor cum molestias rerum?
-				Laudantium, nulla nobis est quod suscipit eveniet ea
-				veniam ab. Aut eos quam sequi eum, consectetur
-				quisquam odio! Quas, deleniti esse beatae
-				exercitationem assumenda aliquid quisquam harum?
-				Molestias ut nesciunt recusandae veritatis. Deserunt
-				fugiat voluptatum labore temporibus eos quisquam
-				dolores enim hic rerum totam nostrum porro quia, sit
-				nulla necessitatibus veritatis culpa harum
-				reprehenderit molestiae. At voluptas incidunt vel
-				repellendus libero. Molestiae, fugiat ducimus? Hic
-				excepturi quae, accusantium dolores eos nostrum
-				laborum alias sit vel expedita ipsum nisi eius
-				commodi.
 			</p>
 		</div>
 	);
