238ad0558ae1bbef196592934aa750465789c2e1
mikemnjovu <mikemnjovu@gmail.com>
2020-04-24 11:25:20 +0200

    Master update

--- src/App.js
+++ src/App.js
@@ -5,30 +5,29 @@ import Footer from './Components/Shared/Footer/Footer';
 import Clock from './Components/Landingpage/Clock/Clock';
 import Topleading from './Components/Landingpage/TopLeading/TopLeading';
 import Infolinks from './Components/Landingpage/InfoLinks/Infolinks';
-import Api from './Components/Api/Api';
 
 import './App.css';
 
 function App() {
-  return (
-    <div className='App'>
-      <div className='Header-Comp'>
-        <Header />
-      </div>
-      <div className='Clock-Comp'>
-        <Clock />
-      </div>
-      <div className='Topleading-Comp'>
-        <Topleading />
-      </div>
-      <div className='Infor-Comp'>
-        <Infolinks />
-      </div>
-      <div className='Footer-Comp'>
-        <Footer />
-      </div>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<div className='Header-Comp'>
+				<Header />
+			</div>
+			<div className='Clock-Comp'>
+				<Clock />
+			</div>
+			<div className='Topleading-Comp'>
+				<Topleading />
+			</div>
+			<div className='Infor-Comp'>
+				<Infolinks />
+			</div>
+			<div className='Footer-Comp'>
+				<Footer />
+			</div>
+		</div>
+	);
 }
 
 export default App;
