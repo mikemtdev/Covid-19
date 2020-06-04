6e012eabc18296d639af2348fac40fa14e331a4d
Mikemnjovu <mikemnjovu@gmail.com>
2020-06-04 21:33:57 +0200

    change to hooks for fetch and DidmountComponent

--- src/Components/Landingpage/Clock/Clock.js
+++ src/Components/Landingpage/Clock/Clock.js
@@ -1,64 +1,64 @@
-import React, { useState } from 'react';
-import './Clock.css';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import "./Clock.css";
+import axios from "axios";
 const Clock = () => {
-	const [Confirmed, setConfirmed] = useState(null);
-	const [Active, setActive] = useState(null);
-	const [Deaths, setDeaths] = useState(null);
-	const [UpDate, setUpDate] = useState(null);
-	const [Recovered, setRecovered] = useState(null);
-	const apiFetchComfirmed = axios
-		.get('https://covid2019-api.herokuapp.com/v2/total')
-		.then(resp => {
-			const confirmedCases = resp.data.data.confirmed;
-			setConfirmed(confirmedCases);
+  const [Confirmed, setConfirmed] = useState(null);
+  const [Active, setActive] = useState(null);
+  const [Deaths, setDeaths] = useState(null);
+  const [UpDate, setUpDate] = useState(null);
+  const [Recovered, setRecovered] = useState(null);
 
-			const lastUpdateDate = resp.data.dt;
-			setUpDate(lastUpdateDate);
+  const fetchDataApi = async () => {
+    const apiFetchComfirmed = await axios
+      .get("https://covid2019-api.herokuapp.com/v2/total")
+      .then((resp) => {
+        const confirmedCases = resp.data.data.confirmed;
+        setConfirmed(confirmedCases);
 
-			const activeCases = resp.data.data.active;
-			setActive(activeCases);
+        const lastUpdateDate = resp.data.dt;
+        setUpDate(lastUpdateDate);
 
-			const recoveredCases = resp.data.data.recovered;
-			setRecovered(recoveredCases);
+        const activeCases = resp.data.data.active;
+        setActive(activeCases);
 
-			const DeathCases = resp.data.data.deaths;
-			setDeaths(DeathCases);
-		})
-		.catch(err => console.log('err'));
+        const recoveredCases = resp.data.data.recovered;
+        setRecovered(recoveredCases);
 
-	return (
-		<div className='clockmain-Container'>
-			<div className='clock-container'>
-				<h1 className='clock-h1'>
-					{Confirmed
-						? `Comfirmed${Confirmed}`
-						: 'Please wait....'}
-				</h1>
-				<h3 className='clock-h1'>
-					{Active
-						? `Infected ${Active}`
-						: 'Please wait....'}
-				</h3>
-				<h3 className='clock-h1'>
-					{Recovered
-						? `Recovered ${Recovered}`
-						: 'Please wait....'}
-				</h3>
-				<h3 className='clock-h1'>
-					{Deaths ? `Deaths ${Deaths}` : 'Please wait....'}
-				</h3>
-				{/* <h2 className='clock-h1'>
+        const DeathCases = resp.data.data.deaths;
+        setDeaths(DeathCases);
+      })
+      .catch((err) => console.log("err"));
+  };
+
+  useEffect(() => {
+    fetchDataApi(Confirmed, Active, Recovered, Deaths, UpDate);
+  }, []);
+
+  return (
+    <div className="clockmain-Container">
+      <div className="clock-container">
+        <h1 className="clock-h1">
+          {Confirmed ? `Comfirmed${Confirmed}` : "Please wait...."}
+        </h1>
+        <h3 className="clock-h1">
+          {Active ? `Infected ${Active}` : "Please wait...."}
+        </h3>
+        <h3 className="clock-h1">
+          {Recovered ? `Recovered ${Recovered}` : "Please wait...."}
+        </h3>
+        <h3 className="clock-h1">
+          {Deaths ? `Deaths ${Deaths}` : "Please wait...."}
+        </h3>
+        {/* <h2 className='clock-h1'>
 					{ `Leading Country `}
 				</h2> */}
 
-				<p className='clock-h1'>
-					<h3>Last Update date</h3>{' '}
-					{UpDate ? UpDate : 'Please wait....'}
-				</p>
-			</div>
-		</div>
-	);
+        <p className="clock-h1">
+          <h3>Last Update date</h3> {UpDate ? UpDate : "Please wait...."}
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Clock;
--- src/Components/Landingpage/TopLeading/TopLeading.js
+++ src/Components/Landingpage/TopLeading/TopLeading.js
@@ -1,25 +1,30 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import './TopLeading.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./TopLeading.css";
 
 const Topleading = () => {
-	const [Country, setCountry] = useState();
-	const apiFetch = axios
-		.get('https://covid2019-api.herokuapp.com/v2/current')
-		.then(resp => {
-			const test = resp.data.data;
-			setCountry(test);
-			console.log(resp.data.data);
-		});
-	const api = {
-		country: 'Zambia',
-		infected: '232',
-		death: '4',
-	};
-	return (
-		<div className='toplead-container'>
-			<div className='toplead-item'>
-				{/* <h3>{Country}</h3>
+  const [Country, setCountry] = useState();
+  const fetchDataApi = async () => {
+    const apiFetch = await axios
+      .get("https://covid2019-api.herokuapp.com/v2/current")
+      .then((resp) => {
+        const test = resp.data.data;
+        setCountry(test);
+        console.log(resp.data.data);
+      });
+  };
+  useEffect(() => {
+    fetchDataApi(Country);
+  }, []);
+  const api = {
+    country: "Zambia",
+    infected: "232",
+    death: "4",
+  };
+  return (
+    <div className="toplead-container">
+      <div className="toplead-item">
+        {/* <h3>{Country}</h3>
 				<p>
 					<i>Infected {api.infected}</i>
 				</p>
@@ -27,22 +32,22 @@ const Topleading = () => {
 					<i>Deaths {api.death}</i>
 				</p> */}
 
-				<ul className='toplead-ul'>
-					{Country
-						? Country.map(item => (
-								<li className='toplead-item' key={item.id}>
-									<h3>{item.location}</h3>
-									<p>Confirmed {item.confirmed}</p>
-									<p>Active {item.active}</p>
-									<p>Recovered {item.recovered}</p>
-									<p>Deaths {item.deaths}</p>
-								</li>
-						  ))
-						: 'Please wait....'}
-				</ul>
-			</div>
-		</div>
-	);
+        <ul className="toplead-ul">
+          {Country
+            ? Country.map((item) => (
+                <li className="toplead-item" key={item.id}>
+                  <h3>{item.location}</h3>
+                  <p>Confirmed {item.confirmed}</p>
+                  <p>Active {item.active}</p>
+                  <p>Recovered {item.recovered}</p>
+                  <p>Deaths {item.deaths}</p>
+                </li>
+              ))
+            : "Please wait...."}
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Topleading;
